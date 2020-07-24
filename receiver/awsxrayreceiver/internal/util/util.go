5e2e083e9a73bae92ed6d3f9a2123346fd02e282
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-07-24 10:05:49 -0700

    Addressed feedbacks
    1. Add Amazon copyright header
    2. Moved references to commit message
    3.

--- receiver/awsxrayreceiver/internal/util/util.go
+++ receiver/awsxrayreceiver/internal/util/util.go
@@ -12,6 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Copyright 2018-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
 package util
 
 import (
@@ -19,9 +28,6 @@ import (
 
 	"go.uber.org/zap"
 )
-
-// Reference for this port:
-// https://github.com/aws/aws-xray-daemon/blob/master/pkg/util/util.go
 
 // ProtocolSeparator is the character used to split the header and body in an
 // X-Ray segment
--- receiver/awsxrayreceiver/receiver.go
+++ receiver/awsxrayreceiver/receiver.go
@@ -12,6 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Copyright 2018-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
 package awsxrayreceiver
 
 import (
@@ -119,8 +128,6 @@ func (x *xrayReceiver) Shutdown(_ context.Context) error {
 	return err
 }
 
-// Reference for this port:
-// https://github.com/aws/aws-xray-daemon/blob/master/cmd/tracing/daemon.go#L257
 func (x *xrayReceiver) read(buf *[]byte) (int, error) {
 	bufVal := *buf
 	rlen, err := x.udpSock.Read(bufVal)
